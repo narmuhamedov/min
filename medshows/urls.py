f3eaee7835b05bd565678e04e8472e7f13d166b3
User <kigradomir@gmail.com>
2022-09-27 15:59:15 +1400

    min django lesson write oop style

--- medshows/urls.py
+++ medshows/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('medshows/', views.show_all_med, name='medshow'),
-    path('medshows/<int:id>/', views.show_all_med_detail, name='medshows_detail'),
-    path('medshows/<int:id>/update/', views.show_update, name='medshows_update'),
-    path('medshows/<int:id>/delete/', views.show_delete, name='medshows_delete'),
-    path('add-shows/', views.add_med_shows, name='medShow'),
+    path('medshows/', views.MedShowListView.as_view(), name='medshow'),
+    path('medshows/<int:id>/', views.MedShowsDetailView.as_view(), name='medshows_detail'),
+    path('medshows/<int:id>/update/', views.MedShowUpdateView.as_view(), name='medshows_update'),
+    path('medshows/<int:id>/delete/', views.MedShowsDeleteView.as_view(), name='medshows_delete'),
+    path('add-shows/', views.MedShowsCreateView.as_view(), name='medShow'),
 ]
--- medshows/views.py
+++ medshows/views.py
@@ -1,42 +1,94 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from . import models, forms
+from django . views import generic
 
-def show_all_med(request):
-    med_img_post = models.MedicalShows.objects.all()
-    return render(request, "medpost.html", {'post':med_img_post})
+class MedShowListView(generic.ListView):
+    template_name = 'medpost.html'
+    queryset = models.MedicalShows.objects.all()
+    def get_queryset(self):
+        return models.MedicalShows.objects.all()
+
+
+# def show_all_med(request):
+#     med_img_post = models.MedicalShows.objects.all()
+#     return render(request, "medpost.html", {'post':med_img_post})
 
 #Получение
-def show_all_med_detail(request, id):
-    shows = get_object_or_404(models.MedicalShows, id=id)
-    return render(request, "medical_post_list_detail.html", {'shows': shows})
+class MedShowsDetailView(generic.DetailView):
+    template_name = 'medical_post_list_detail.html'
+
+    def get_object(self, **kwargs):
+        medshows_id = self.kwargs.get('id')
+        return get_object_or_404(models.MedicalShows, id=medshows_id)
+
+
+# def show_all_med_detail(request, id):
+#     shows = get_object_or_404(models.MedicalShows, id=id)
+#     return render(request, "medical_post_list_detail.html", {'shows': shows})
 
 #Добавление
-def add_med_shows(request):
-    method = request.method
-    if method == "POST":
-        form = forms.MedShowForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('med show created')
-    else:
-        form = forms.MedShowForm()
-    return render(request, "add_med_show_form.html", {'med_form': form})
+class MedShowsCreateView(generic.CreateView):
+    template_name = 'add_med_show_form.html'
+    form_class = forms.MedShowForm
+    queryset = models.MedicalShows.objects.all()
+    success_url = '/medshows/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(MedShowsCreateView, self).form_valid(form=form)
+
+
+
+# def add_med_shows(request):
+#     method = request.method
+#     if method == "POST":
+#         form = forms.MedShowForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('med show created')
+#     else:
+#         form = forms.MedShowForm()
+#     return render(request, "add_med_show_form.html", {'med_form': form})
 
 #Обновление
-def show_update(request, id):
-    show_object = get_object_or_404(models.MedicalShows, id=id)
-    if request.method == 'POST':
-        form = forms.MedShowForm(instance=show_object, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Show good update')
-    else:
-        form = forms.MedShowForm(instance=show_object)
-    return render(request, 'show_update.html', {'form': form, 'object': show_object})
+class MedShowUpdateView(generic.UpdateView):
+    template_name = 'show_update.html'
+    form_class = forms.MedShowForm
+    success_url = '/medshows/'
+
+    def get_object(self, **kwargs):
+        medshows_id = self.kwargs.get('id')
+        return get_object_or_404(models.MedicalShows, id=medshows_id)
+    def form_valid(self, form):
+        return super(MedShowUpdateView,self).form_valid(form=form)
+
+
+
+
+
+
+# def show_update(request, id):
+#     show_object = get_object_or_404(models.MedicalShows, id=id)
+#     if request.method == 'POST':
+#         form = forms.MedShowForm(instance=show_object, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Show good update')
+#     else:
+#         form = forms.MedShowForm(instance=show_object)
+#     return render(request, 'show_update.html', {'form': form, 'object': show_object})
 
 #Удаление
-def show_delete(request, id):
-    show_object = get_object_or_404(models.MedicalShows, id=id)
-    show_object.delete()
-    return HttpResponse('Med Show deleted')
+class MedShowsDeleteView(generic.DeleteView):
+    template_name = 'confirm_delete.html'
+    success_url = '/medshows/'
+
+    def get_object(self, **kwargs):
+        show_id = self.kwargs.get('id')
+        return get_object_or_404(models.MedicalShows,id=show_id)
+
+# def show_delete(request, id):
+#     show_object = get_object_or_404(models.MedicalShows, id=id)
+#     show_object.delete()
+#     return HttpResponse('Med Show deleted')
