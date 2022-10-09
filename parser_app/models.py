77bfae82e31ab4fd3952c5b485050e5517b43e9c
User <kigradomir@gmail.com>
2022-10-10 08:08:20 +1400

    ignore

--- parser_app/models.py
+++ parser_app/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class AllMedApartment(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='')
+
+    def __str__(self):
+        return self.name
--- parser_app/parser.py
+++ parser_app/parser.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 from django.views.decorators.csrf import csrf_exempt
 
-HOST = "http://medcenter-kgma.kg/"
-URL = "http://medcenter-kgma.kg/doctors.html"
+HOST = "https://zdorovie-lab.kg/"
+URL = "https://zdorovie-lab.kg/%d1%81%d0%bf%d0%b5%d1%86%d0%b8%d0%b0%d0%bb%d0%b8%d1%81%d1%82%d1%8b/"
 
 HEADERS = {
-    'Accept': 'ext/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'
 }
 
@@ -19,14 +19,14 @@ def get_html(url, params=''):
 @csrf_exempt
 def get_data(html):
     soup = BeautifulSoup(html, 'html.parser')
-    items = soup.find_all('div', class_='blog')
+    items = soup.find_all('div', class_='row specialists_list')
     mediki = []
 
     for item in items:
         mediki.append(
             {
-                'name': item.find('div', class_='page-header').get_text(),
-                'image': item.find('div', class_='cat-left').find('img').get('src')
+                'name': item.find('div', class_='specialists_item_title').get_text(),
+                'image': item.find('div', class_='specialists_item_img').find('img').get('src')
             }
         )
         return mediki
